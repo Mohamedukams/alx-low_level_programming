c2219ec864e96a043d7009543e72cd7bf4114b1b
Mohamed <mohamed.ams114@gmail.com>
2023-03-03 10:32:19 -0800

    one time push

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints putchar function
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+
+	_putchar();
+	printf("\n");
+	return (0);
+
+}
